6d4a93ad5d98f9e55eeac2e0baa0321380a81a2f
Jacob <jacobm3@gmail.com>
2019-05-15 23:18:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ module "s3_simple" {
   version = "1.0"
   bucket_name = "jmartinson-delete-me-2347239749823792438"
 }
+  
+module "s3_unapproved" {
+  source  = "app.terraform.io/jacobm3/s3_unapproved/aws"
+  version = "1.0"
+  bucket_name = "jmartinson-delete-me-2347239749823798"
+}
 
 #resource "aws_s3_bucket" "b" {
 #  bucket = "jmartinson-d41d8cd98f00b204e9800998ecf8427e"
